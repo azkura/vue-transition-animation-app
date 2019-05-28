db26cb6317ac7fbc327aee977ba57b6ad61518ac
az <azizkoura@hotmail.com>
2019-05-28 22:26:36 +0200

    animation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+                <hr>
+                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+                <br><br>
+                <transition name="fade">
+                  <div class="alert alert-info" v-if="show">this is info</div>
+                </transition>
+                <transition name="slide">
+                  <div class="alert alert-info" v-if="show">this is info</div>
+                </transition>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        data() {
+            return {
+              show: false
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.fade-enter {
+  opacity: 0;
 }
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-leave {
+
+}
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
+}
+.slide-enter {
+
+}
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+
+}
+.slide-leave {
+
+}
+.slide-leave-active {
+  animation: slide-out ease-out forwards;
+
+}
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
+
 </style>
